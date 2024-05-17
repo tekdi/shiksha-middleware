9fe1e2e89e6763f514848b476d69a3352e355d14
poojakarma <pooja.karma@tekditechnologies.com>
2024-05-17 10:53:04 +0530

    separete config file for api route

---
+++ src/common/middleware/apiConfig.ts
@@ -0,0 +1,37 @@
+export const apiMappings = {
+    identifier1: {
+        // microservice: 'http://3.109.46.84:4000',
+        microservice: 'http://localhost:3000',
+        endpoint: 'event/v1/{dynamicParam}',
+        permission: 'event-read',
+        method: 'GET'
+    },
+    identifier2: {
+        // microservice: 'http://3.109.46.84:4000',
+        microservice: 'http://localhost:3000',
+        endpoint: 'event/v1/{dynamicParam}',
+        permission: 'event-delete',
+        method: 'DELETE'
+    },
+    identifier3: {
+        // microservice: 'http://3.109.46.84:4000',
+        microservice: 'http://localhost:3000',
+        endpoint: 'event/v1/create',
+        permission: 'event-create',
+        method: 'POST',
+    },
+    identifier4: {
+        // microservice: 'http://3.109.46.84:4000',
+        microservice: 'http://localhost:3000',
+        endpoint: 'event/v1/{dynamicParam}',
+        permission: 'event-update',
+        method: 'PATCH',
+    },
+    identifier5: {
+        // microservice: 'http://3.109.46.84:4000',
+        microservice: 'http://localhost:3000',
+        endpoint: 'event/v1/list',
+        permission: 'event-read',
+        method: 'POST',
+    }
+};
