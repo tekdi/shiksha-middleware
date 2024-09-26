fa5d2388fe76eaf7d88b3dcc4198c78f7ae86f5a
Kshitija Kadam <65657373+Xitija@users.noreply.github.com>
2024-09-26 13:24:10 +0530

    chore : clean up and merge

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,23 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as dotenv from 'dotenv';
+import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   dotenv.config(); // Load environment variables from .env file
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const corsOptions = {
+    // origin: configService.get<string>('CORS_ORIGIN'),
+    // methods: configService.get<string>('CORS_METHODS') || 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    // allowedHeaders: configService.get<string>('CORS_ALLOWED_HEADERS') || 'Content-Type, Accept',
+    // credentials: configService.get<boolean>('CORS_CREDENTIALS') || true,
+  };
+  
+  app.enableCors(corsOptions);
+  app.use(helmet());
+  app.use(helmet.hidePoweredBy());
   const config = new DocumentBuilder()
     .setTitle('Middleware  APIs')
     .setDescription('The Middlware service')
