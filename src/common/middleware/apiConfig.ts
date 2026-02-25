a4cb9133da6baa9436c0869c13225f2439cd5511
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2026-02-25 12:29:37 +0530

    TASK #00000 : coupon APIs changes

--- src/common/middleware/apiConfig.ts
+++ src/common/middleware/apiConfig.ts
@@ -1514,6 +1514,11 @@ export const apiList = {
       ROLE_CHECK: rolesGroup.common,
     },
   }),
+  '/user/v1/coupons/list': createRouteObject({
+    post: {
+      ROLE_CHECK: rolesGroup.superadmin,
+    },
+  }),
   '/user/v1/coupons': createRouteObject({
     post: {
       ROLE_CHECK: rolesGroup.superadmin,
@@ -1540,7 +1545,12 @@ export const apiList = {
     },
   }),
   '/user/v1/coupons/:id': createRouteObject({
-    put: {
+    patch: {
+      ROLE_CHECK: rolesGroup.superadmin,
+    },
+  }),
+  '/user/v1/coupons/delete/:id': createRouteObject({
+    delete: {
       ROLE_CHECK: rolesGroup.superadmin,
     },
   }),
