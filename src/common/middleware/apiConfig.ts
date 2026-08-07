1cdd9902fb69ea6f663c21669fc6f55ac08f6dc6
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2026-08-07 16:48:56 +0530

    TASK #00000 : Add alp admin in roles

--- src/common/middleware/apiConfig.ts
+++ src/common/middleware/apiConfig.ts
@@ -64,7 +64,8 @@ const rolesGroup = {
   common: ['admin', 'regional_admin', 'student'],
   regional_admin: ['regional_admin'],
   student: ['student'],
-  superadmin: ['admin'],
+  // Added alp_program_admin role to superadmin group for testing purpose, will be removed once alp_program_admin role is added to the user_roles_mapping table
+  superadmin: ['admin', 'alp_program_admin'],
   superadmin_regional_admin: ['admin', 'regional_admin'],
   student_regional_admin: ['student', 'regional_admin'],
   superadmin_regional_admin_student: ['admin', 'regional_admin', 'student'],
