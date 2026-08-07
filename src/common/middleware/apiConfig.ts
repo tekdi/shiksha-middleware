506cd9d881c8e27d480344f392b9871af6664065
Mahesh Mahajan <mahajanmahesh935@gmail.com>
2026-08-07 16:57:25 +0530

    TASK #00000 : Add alp admin

--- src/common/middleware/apiConfig.ts
+++ src/common/middleware/apiConfig.ts
@@ -67,9 +67,14 @@ const rolesGroup = {
   // Added alp_program_admin role to superadmin group for testing purpose, will be removed once alp_program_admin role is added to the user_roles_mapping table
   superadmin: ['admin', 'alp_program_admin'],
   superadmin_regional_admin: ['admin', 'regional_admin', 'alp_program_admin'],
-  student_regional_admin: ['student', 'regional_admin'],
-  superadmin_regional_admin_student: ['admin', 'regional_admin', 'student'],
-  superadmin_student: ['admin', 'student'],
+  student_regional_admin: ['student', 'regional_admin', 'alp_program_admin'],
+  superadmin_regional_admin_student: [
+    'admin',
+    'regional_admin',
+    'student',
+    'alp_program_admin',
+  ],
+  superadmin_student: ['admin', 'student', 'alp_program_admin'],
 };
 const createPrivilegeGroup = (entity: string) => {
   return {
